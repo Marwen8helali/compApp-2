180f31141900d02bf5d9b90103af4236edde3c56
Marwen8helali <marwen.helali@gmail.com>
2021-03-16 12:56:39 +0100

    first commit

---
+++ dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:8
+COPY target/compApp-2-0.0.1-SNAPSHOT.jar app.jar
+EXPOSE 8081
+CMD java -jar app.jar
---
+++ src/test/java/com/Marwen_PFE_BFIgroupe/CompApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Marwen_PFE_BFIgroupe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
